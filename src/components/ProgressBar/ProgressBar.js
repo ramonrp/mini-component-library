f786457e6e96d5171e788d72de43e0cfec56e377
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-05-07 18:09:51 +0200

    "finished first workshop"

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,57 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const styleWrapper = {
+  small:{
+    "--height": "8px",
+    "--border-radius": "4px",
+    "--padding":"0"
+  },
+  medium:{
+    "--height": "12px",
+    "--border-radius": "4px",
+    "--padding":"0"
+  },
+  large:{
+    "--height": "24px",
+    "--border-radius": "8px",
+    "--padding":"4px"
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+    if(value<0 || value >100){
+    throw new Error("value should be within 0 and 100");
+  }
+  const wrapperStyle = styleWrapper[size]
+
+  if(!wraooerStyle){
+    throw new Error (`size prop not valid, ${size}`, you need to chose a valid one)
+  }
+    return <Wrapper style={wrapperStyle}>
+    <Progress
+      value={value}
+      role="progressbar" 
+      aria-valuenow={value} 
+      aria-valuemin="0" 
+      aria-valuemax="100">
+    </Progress>
+  </Wrapper>;
 };
 
-export default ProgressBar;
+const Wrapper = styled.div`
+  height: var(--height);
+  border-radius:var(--border-radius);
+  padding:var(--padding);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`
+
+const Progress = styled.div`
+  background-color:${COLORS.primary};
+  height:100%;
+  width:${props=>props.value}%;
+  border-top-left-radius:4px;
+  border-bottom-left-radius:4px;
+  border-top-right-radius: ${props=>props.value>98 ? 4: 0 }px;
+  border-bottom-right-radius: ${props=>props.value>98 ? 4: 0 }px;
+`
