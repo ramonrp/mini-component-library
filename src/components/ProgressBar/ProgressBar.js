e4b85eace3adb2c2191ac615e49498e745e63297
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-05-07 18:13:22 +0200

    Corrected export ProgressBar.js

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -29,8 +29,8 @@ const ProgressBar = ({ value, size }) => {
   }
   const wrapperStyle = styleWrapper[size]
 
-  if(!wraooerStyle){
-    throw new Error (`size prop not valid, ${size}`, you need to chose a valid one)
+  if(!wrapperStyle){
+    throw new Error (`size prop not valid, ${size}, you need to chose a valid one`)
   }
     return <Wrapper style={wrapperStyle}>
     <Progress
@@ -59,3 +59,42 @@ const Progress = styled.div`
   border-top-right-radius: ${props=>props.value>98 ? 4: 0 }px;
   border-bottom-right-radius: ${props=>props.value>98 ? 4: 0 }px;
 `
+
+
+
+export default ProgressBar;
+
+// import React from 'react';
+// import styled from 'styled-components';
+// import { NoEmitOnErrorsPlugin } from 'webpack';
+
+// import { COLORS } from '../../constants';
+// import VisuallyHidden from '../VisuallyHidden';
+
+
+
+// const ProgressBar = ({ value, size }) => {
+
+
+  
+
+
+// const Wrapper = styled.div`
+//   width: 370px;
+//   height: 12px;
+//   border: 0px solid ${COLORS.transparentGray15};
+//   border-radius:8px;
+//   padding:0;
+//   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+//   padding:0;
+// `
+
+// const Progress = styled.div`
+//   background-color:${COLORS.primary};
+//   height:100%;
+//   width:${props=>props.value}%;
+//   border-top-left-radius:12px;
+//   border-bottom-left-radius:12px;
+// `
+
+// export default ProgressBar;
