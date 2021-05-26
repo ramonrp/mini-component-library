90986cb7432636dbb8d3351941eba0890b130fde
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-05-26 19:46:32 +0200

    Added Select Component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,53 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper>
+    <NativeSelect value={value} onChange={onChange}>
       {children}
-    </select>
+    </NativeSelect>
+    <PresentationalBit>
+      {displayedValue}
+      <Icon id="chevron-down" size="24" strokeWidth="1"></Icon>
+    </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position:relative;
+  width: max-content;
+`;
+
+
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+`
+const PresentationalBit = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-size: ${16/16}rem;
+  border-radius: 8px;
+  padding: 12px 16px;
+  padding-right: 12px;
+  display: flex;
+  align-items: center;
+  gap: 18px;
+
+  ${NativeSelect}:focus + &{
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+  ${NativeSelect}:hover + &{
+    color:${COLORS.black};
+  }
+`;
 
 export default Select;
